4b1b50a2c5d879959f95bf783bda0ece1256dee1
gjrhksan004-cmd <gjrhksan004@gmail.com>
2026-01-22 20:34:03 +0900

    [Bronze V] Title: A+B - 3, Time: 116 ms, Memory: 14204 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,26 @@
+import java.io.*;          
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int T = Integer.parseInt(br.readLine());
+
+
+        for (int i = 0; i < T; i++) {
+
+            String line = br.readLine();
+
+
+            StringTokenizer st = new StringTokenizer(line);
+
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+
+            System.out.println(A + B);
+        }
+    }
+}
