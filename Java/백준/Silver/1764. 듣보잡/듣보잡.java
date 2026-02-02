adb5732d804456b362082587b491aa81e6e1eb06
gjrhksan004-cmd <gjrhksan004@gmail.com>
2026-02-02 21:00:26 +0900

    [Silver IV] Title: 듣보잡, Time: 260 ms, Memory: 26516 KB -BaekjoonHub

---
+++ Java/백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> unheard = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            unheard.add(br.readLine());
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            String name = br.readLine();
+            if (unheard.contains(name)) {
+                result.add(name);
+            }
+        }
+
+        Collections.sort(result);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(result.size()).append("\n");
+        for (String s : result) {
+            sb.append(s).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
