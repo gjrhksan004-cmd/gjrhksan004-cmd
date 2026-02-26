097645e1520e76df2de48b5e138d6b3a6609f515
gjrhksan004-cmd <gjrhksan004@gmail.com>
2026-02-26 16:46:58 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 112 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+
+        Stack<String> stack = new Stack<>();
+
+
+        for(int i=0;i<testCase;i++) {
+            String input = br.readLine();
+            String[] arr = input.split(" ");
+            for (int j = 0; j < arr.length; j++)
+                stack.push(arr[j]);
+
+
+            System.out.print("Case #"+(i+1)+": ");
+            while(!stack.isEmpty()){
+                System.out.print(stack.pop());
+                System.out.print(" ");
+                if(stack.isEmpty())
+                    System.out.println();
+            }
+        }
+
+
+
+
+    }
+}
